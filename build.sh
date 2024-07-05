e6236150bafe03031eb698adfb98c8dd7f40e5ae
James-Livesey <jamesliveg@gmail.com>
2024-07-05 19:37:52 +0100

    Update example to use the latest version of Voxel

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@ mkdir -p firmware/included
 
 cp lib/voxel/dist/libvoxel.h firmware/included/libvoxel.h
 
-pio run
+if [ "$1" == "--upload" ]; then
+    pio run -t upload -e ${2:-um_feathers3}
+else
+    pio run
+fi
